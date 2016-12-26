6e0b13865a57c967abc40f1afca99610c67e7226
Andrei Diaconu <andrei_di@yahoo.com>
2016-12-27 00:57:23 +0200

    Added comments

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 "use strict";
 
+/**
+*
+* WARNING: Uploading very large files, 
+* or relatively small files in large numbers very quickly, 
+* can cause your application to run out of memory when memory storage is used.
+*
+**/
+
 const express	= require('express');
 const app	    = express();
 const crypto    = require('crypto');
@@ -8,18 +16,26 @@ const mime      = require('mime');
 const morgan    = require('morgan');
 const port      = process.env.PORT || 3113;
 let timeInMs    = Date.now();
-let storage	    = multer.diskStorage({
+let storage	= multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads');
+    cb(null, './uploads'); /* destination folder for uploads. 
+    **Note: in order for uploads to work, 
+    you need to create a folder named "uploads" in the root directory */
   },
   filename: function (req, file, cb) {
+	// function to rename the uploaded file
     crypto.pseudoRandomBytes(16, function (err, raw) {
-      cb(null, raw.toString('hex') + timeInMs + '.' + mime.extension(file.mimetype));
-    });
+      cb(null, raw.toString('hex') + timeInMs + '.' + mime.extension(file.mimetype)); 	 
+    }); /* this will generate a pseudo random filename,
+    	* but will still use the same file extension 
+	* to remove the file extension, delete ' + '.' + mime.extension(file.mimetype)) '
+	*/
+    
   }
 });
 let upload = multer({ storage : storage }).single('fileUpload');
 
+// use morgan middleware to log HTTP requests
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/client'));
@@ -31,7 +47,7 @@ app.get('/',function(req,res){
 app.post('/api/upload',function(req,res){
 	upload(req,res,function(err) {
 		if(err) {
-			return res.end("Error uploading file.");
+			return res.end("<h1>Error uploading file.</h1>");
 		}
 		res.end("<h1>File uploaded successfully</h1>");        
 	});
